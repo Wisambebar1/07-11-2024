186fe356b4c99c982bfe479bf522ac6165005b85
Wisambebar1 <wisambebar@gmail.com>
2024-11-07 13:29:08 +0200

    aa

--- hooks_1/src/Components/Person.jsx
+++ hooks_1/src/Components/Person.jsx
@@ -1,5 +1,6 @@
 const Person=(props)=>{
     return (<article className="person">
+
         <img src={props.person.image} alt={props.person.name} className="img"></img>
         <div>
             <h4>{props.person.name}</h4>
