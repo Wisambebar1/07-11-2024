526c85005bceec7f821076720293d25e2e1691c6
Wisambebar1 <wisambebar@gmail.com>
2024-11-07 15:23:46 +0200

    done

---
+++ reviews_project/reviews/src/Components/Data.jsx
@@ -0,0 +1,14 @@
+const reviews=[
+    {id:1,name:"susuan smith",job: "web developer",
+        image:"https://pixabay.com/images/search/random/"
+        ,text:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
+    {id:2,name:"anna johnson",job: "web designer",
+        image:"https://pixabay.com/images/search/random/"
+        ,text:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
+    {id:3,name:"peter jones",job: "intern",
+        image:"https://pixabay.com/images/search/random/",
+        text:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"},
+    {id:4,name:"bill anderson",job: "boss",
+        image:"https://pixabay.com/images/search/random/"
+        ,text:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}]
+export default reviews
